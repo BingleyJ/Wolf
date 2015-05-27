d2200e508cbb30a8707fc336d5a2c40fef8965aa
Jay Bay <gratholio@gmail.com>
2015-05-27 19:31:36 -0400

    IO

--- src/IO.java
+++ src/IO.java
@@ -1,4 +1,21 @@
 
-public class IO {
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class IO {
+	ArrayList<Object> level = new ArrayList<Object>();
+	List<List<Object>> levelList = new ArrayList<List<Object>>();
+	
+	public IO(){
+		
+	}
+	
+	public void save(){
+		
+	}
+	
+	public void load(){
+		
+	}
 }
